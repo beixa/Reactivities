12af36fca4ccbc18234ebb9b84caca148340a4d9
Ikerbeitia <beitia.iker@gmail.com>
2020-06-03 10:44:29 +0200

    End of Section 13

--- Application/Validators/ValidatorExtensions.cs
+++ Application/Validators/ValidatorExtensions.cs
@@ -8,7 +8,7 @@ namespace Application.Validators
         {
             var options =  ruleBuilder
                 .NotEmpty()
-                .MinimumLength(6).WithMessage("Password must be art least 6 characters")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters")
                 .Matches("[A-Z]").WithMessage("Password must contain 1 uppercase letter")
                 .Matches("[a-z]").WithMessage("Password must have at least 1 lowercase character")
                 .Matches("[0-9]").WithMessage("Password must contain a number")
